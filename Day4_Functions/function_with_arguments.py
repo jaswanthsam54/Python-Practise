96979275bfd39f70a39c569792c68b0aebeca708
Jaswanth Saminepally <saminepallyjaswanth@gmail.com>
2025-08-22 08:33:05 +0530

    Add files via upload

---
+++ Day4_Functions/function_with_arguments.py
@@ -0,0 +1,7 @@
+#function_with_arguments.py
+def funct(name,ending):
+    print("Good Morning", name)
+    print(ending)
+funct("Harry","Thankyou")
+funct("Pranav","Thankyou")
+funct("Asish","Thankyou")
